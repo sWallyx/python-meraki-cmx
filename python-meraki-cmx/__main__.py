4c7793bd1e5547644d6d815ecd4bd04069212704
root <root@ip-172-31-19-211.eu-west-1.compute.internal>
2019-04-08 13:15:09 +0000

    Receiver working

--- python-meraki-cmx/__main__.py
+++ python-meraki-cmx/__main__.py
@@ -1,38 +1,66 @@
+from pprint import pprint
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
 from flask_restful import Resource, Api
 import json
 from flask_jsonpify import jsonify
+import sys, getopt
 
 app = Flask(__name__)
 api = Api(app)
 
 validator = '8edf4ae18d782e5ac90512bc7db3850038879397'
-
+secret = 'nosfnoqf3276293r'
+version = '2.0'
 CORS(app)
+
+def save_data(data):
+    print("---- SAVING CMX DATA ----")
+    # CHANGE ME - send 'data' to a database or storage system
+    pprint(data, indent=1)
 
 @app.route('/', methods=['GET'])
 def get_validator():
     print("validator sent to: ",request.environ['REMOTE_ADDR'])
     return validator
 
-class Events(Resource):
-	def post(self):
-		print (self);
-		return validator
+# Accept CMX JSON POST
+@app.route('/', methods=['POST'])
+def get_cmxJSON():
+    if not request.json or not 'data' in request.json:
+        return("invalid data",400)
+    cmxdata = request.json
+    #pprint(cmxdata, indent=1)
+    print("Received POST from ",request.environ['REMOTE_ADDR'])
 
-	def get(self):
-		print("validator sent to: ",request.environ['REMOTE_ADDR'])
-    		return validator
+    # Verify secret
+    if cmxdata['secret'] != secret:
+        print("secret invalid:", cmxdata['secret'])
+        return("invalid secret",403)
+    else:
+        print("secret verified: ", cmxdata['secret'])
 
-class Events_Info(Resource):
-	def post(self, events_info):
-		print(self)
-		print(events_info)
+    # Verify version
+    if cmxdata['version'] != version:
+        print("invalid version")
+        return("invalid version",400)
+    else:
+        print("version verified: ", cmxdata['version'])
 
+    # Determine device type
+    if cmxdata['type'] == "DevicesSeen":
+        print("WiFi Devices Seen")
+    elif cmxdata['type'] == "BluetoothDevicesSeen":
+        print("Bluetooth Devices Seen")
+    else:
+        print("Unknown Device 'type'")
+        return("invalid device type",403)
 
-api.add_resource(Events, '/events')
-api.add_resource(Events_Info, '/events/<events_info>')
+    # Do something with data (commit to database)
+    save_data(cmxdata)
+
+    # Return success message
+    return "CMX POST Received"
 
 if __name__ == '__main__':
 	app.run(host='0.0.0.0',port=5002)
